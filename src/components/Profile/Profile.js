c34a0da228f77eb15e9304a14df93d52646f2239
Gonzalo Quiroz <kiyegoo@gmail.com>
2018-12-19 00:21:59 -0600

    Se agrega componente para imagen de perfil y se realiza peticion get para datos de usuario

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
-import { Col, Row } from "antd";
+import { Col, Row, Form, Input, Button } from "antd";
+import Avatar from "./Avatar";
+import axios from "axios";
+
+const FormItem = Form.Item;
+const { TextArea } = Input;
 
 class Profile extends Component {
   constructor() {
@@ -8,29 +13,122 @@ class Profile extends Component {
       form: {
         name: "",
         last_name: "",
-        email: "",
         password: "",
-        confirmPassword: ""
+        description: ""
       }
     };
   }
 
+  componentWillMount() {
+    const base_url = `http://localhost:3000/api/auth/${this.props.user._id}`;
+    const headers = {
+      "Content-Type": "application/json",
+      "x-access-token": localStorage.getItem("token")
+    };
+
+    axios
+      .get(base_url, { headers })
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
   handleChange = e => {
-    let { form, dirty } = this.state;
+    let { form } = this.state;
     let field = e.target.name;
     form[field] = e.target.value;
-    dirty = true;
-    this.setState({ form, dirty });
+    //console.log(form);
+
+    this.setState({ form });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    let { form } = this.state;
+    const base_url = `http://localhost:3000/api/auth/${this.props.user._id}`;
+    const headers = {
+      "Content-Type": "application/json",
+      "x-access-token": localStorage.getItem("token")
+    };
+
+    console.log(this.state.form);
+    console.log(base_url);
+    console.log(headers);
+
+    axios
+      .patch(base_url, form, headers)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   };
 
   render() {
-    console.log(this.props);
-
     return (
       <div className="container">
         <Row>
           <Col span={8} offset={8}>
             <h1 style={{ textAlign: "center" }}>Profile</h1>
+            <Avatar />
+            <Form onSubmit={this.handleSubmit} className="login-form">
+              <FormItem>
+                <Input
+                  type="text"
+                  placeholder="Nombre"
+                  name="name"
+                  onChange={this.handleChange}
+                  value={this.props.user.name}
+                />
+              </FormItem>
+              <FormItem>
+                <Input
+                  type="text"
+                  placeholder="Apellido"
+                  name="last_name"
+                  onChange={this.handleChange}
+                  value={this.props.user.last_name}
+                />
+              </FormItem>
+              <FormItem>
+                <Input
+                  type="text"
+                  placeholder="Email"
+                  name="email"
+                  disabled
+                  value={this.props.user.email}
+                />
+              </FormItem>
+              <FormItem>
+                <Input
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  onChange={this.handleChange}
+                />
+              </FormItem>
+              <FormItem>
+                <TextArea
+                  rows={3}
+                  placeholder="Escribe algo sobre ti"
+                  name="description"
+                  onChange={this.handleChange}
+                />
+              </FormItem>
+              <FormItem>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  className="login-form-button"
+                >
+                  Guardar
+                </Button>
+              </FormItem>
+            </Form>
           </Col>
         </Row>
       </div>
